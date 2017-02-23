c7b90b6158a796ac1e9d9a2db0c78254c11ea601
Michel Casabianca <casa@sweetohm.net>
2017-02-23 10:59:21 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,37 @@
+Xspec Status
+============
+
+- Project:  <http://github.com/c4s4/xspec-status>
+- Releases: <http://github.com/c4s4/xspec-status/releases>
+
+When you run [Xspec](https://github.com/expath/xspec), it prints a status on
+the console and a report in XML files, but the return value doesn't tell if
+tests were on error. Thus it is not possible to stop a build with an error
+on test failure.
+
+This tool parses the Xspec test report and returns the number of errors as
+status code. Thus the call will be on error and will make your build fail
+on Xspec test failure.
+
+Installation
+------------
+
+Unzip the archive, copy the binary for your platform (in the *bin* directory)
+somewhere in your *PATH* and rename it *xspec-status* (or *xspec-status.exe*
+on Windows).
+
+Usage
+-----
+
+To check Xspec test suite success, you must pass XML Xspec test result files on
+command line. Thus, you might run:
+
+    $ xspec-status xspec/*-result.xml
+    Parsing report file 'build/xspec/main-result.xml'...
+    Parsing report file 'build/xspec/meta-result.xml'...
+    Errors: 0
+
+This will print parsed files, the number of errors and will exit with the
+number of errors as return value.
+
+*Enjoy!*
---
+++ xspec-status.go
@@ -0,0 +1,63 @@
+//usr/bin/env go run $0 "$@" ; exit
+
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Test struct {
+	Success bool `xml:"successful,attr"`
+}
+
+type Scenario struct {
+	Label string `xml:"label"`
+	Test  *Test  `xml:"test"`
+}
+
+func (s *Scenario) String() string {
+	return fmt.Sprintf("[Scenario label='%s' success='%v']", s.Label, s.Test.Success)
+}
+
+type Report struct {
+	Scenarios []*Scenario `xml:"scenario"`
+}
+
+func (r *Report) Errors() int {
+	errors := 0
+	for _, scenario := range r.Scenarios {
+		if !scenario.Test.Success {
+			errors++
+		}
+	}
+	return errors
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("You must pass Xspec report files on command line\n")
+		os.Exit(1)
+	}
+	files := os.Args[1:]
+	errors := 0
+	for _, file := range files {
+		fmt.Printf("Parsing report file '%s'\n", file)
+		source, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Printf("Could not read file '%s': %v\n", file, err)
+			os.Exit(2)
+		}
+		var report Report
+		err = xml.Unmarshal(source, &report)
+		if err != nil {
+			fmt.Printf("Could parse file '%s': %v\n", file, err)
+			os.Exit(3)
+		}
+		errors += report.Errors()
+	}
+	fmt.Println("Errors:", errors)
+	os.Exit(errors)
+}
